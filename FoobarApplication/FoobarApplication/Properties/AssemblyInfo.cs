32a38c319957cdd2143f571a6ec7f28e4662c6ac
Joao Matos Silva <joao@kspace.pt>
2015-09-16 23:38:22 +0100

    Added BuildScript and shared assembly info

--- FoobarApplication/FoobarApplication/Properties/AssemblyInfo.cs
+++ FoobarApplication/FoobarApplication/Properties/AssemblyInfo.cs
@@ -22,14 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c319c704-26e1-4421-bba0-99a75ecf81ad")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FoobarApplication/build.fsx
@@ -0,0 +1,75 @@
+// include Fake lib
+#r @".tools\FAKE\tools\FakeLib.dll"
+
+open Fake
+open Fake.AssemblyInfoFile
+
+RestorePackages()
+
+// Directories
+let buildDir  = @".\.build\"
+let testDir   = @".\.test\"
+let deployDir = @".\.deploy\"
+
+// tools
+//let nunitVersion = GetPackageVersion packagesDir "NUnit.Runners"
+//let nunitPath = @".tools\NUnit.Runners\"
+let fxCopRoot = @".\Tools\FxCop\FxCopCmd.exe"
+    
+let buildMode = getBuildParamOrDefault "buildMode" "Release"
+
+// version info
+let buildNumber =
+  match buildServer with
+  | TeamCity -> buildVersion
+  | _ -> "0"
+
+let version = "1.1." + buildNumber
+
+// Targets
+Target "Clean" (fun _ -> 
+    CleanDirs [buildDir; testDir; deployDir;]
+)
+
+Target "WriteAssemblyInfo" (fun _ ->
+    CreateCSharpAssemblyInfo "./AssemblyInfo_Shared.cs"
+        [Attribute.Version version
+         Attribute.FileVersion version]
+)
+
+Target "CompileApp" (fun _ ->    
+    !! @"**\*.csproj" 
+      |> MSBuildReleaseExt null [ ("RunOctoPack", "true"); ("OctoPackEnforceAddingFiles","true"); ("OctoPackPackageVersion", version) ] "Build" 
+      |> Log "AppBuild-Output: "
+)
+
+
+//TODO: Build project on splitted folders and zip them acordanly.
+Target "Zip" (fun _ ->
+    !! (buildDir + "\_PublishedWebsites\**\*.*")
+        -- "*.zip" 
+        |> Zip buildDir (deployDir + "CityOfLove.Web." + version + ".zip")
+)
+
+Target "Default" (fun _ ->
+    trace "Built!"
+)
+
+Target "Release" (fun _ ->
+    trace "Release Target! You've nailed it"
+)
+
+
+// Dependencies
+"Clean"
+  //==> "WriteAssemblyInfo"
+  ==> "CompileApp" 
+  //==> "CompileTest"
+  //==> "FxCop"
+  //==> "NUnitTest"  
+  ==> "Default"
+  ==> "Zip"
+  ==> "Release"
+ 
+// start build
+RunTargetOrDefault "Default"
